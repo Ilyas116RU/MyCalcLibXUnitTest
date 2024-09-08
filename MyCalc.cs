0cde7d2ab0bfb26be0c2c0fbaaa4dd29ba34020f
Ilyas <frostastoru22998@mail.ru>
2024-09-08 17:20:41 +0300

    Добавьте файлы проекта.

---
+++ MyCalc.cs
@@ -0,0 +1,25 @@
+﻿namespace MyCalcLib
+{
+    public class MyCalc
+    {
+        public int Sum(int x, int y) 
+        { 
+            return x + y; 
+        }
+
+        public int Multiplication(int xX, int yY) 
+        {
+            return xX * yY;
+        }
+
+        public int Divide(int Xx, int Yy) 
+        { 
+            return Xx / Yy;
+        }
+
+        public int Subtract(int Xx, int Yy)
+        {
+            return Xx - Yy;
+        }
+    }
+}
